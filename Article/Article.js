6e269eb2984b905afad71f33a40c91c9bdb0241c
Ciphergyst <Jeremy McGrath>
2019-09-09 23:40:53 -0700

    first commit

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,15 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+function createNewsFeed(title, date, firstParagraph, secondParagraph, thirdParagraph) {
+  const feedTitle = document.createElement("title"); 
+  feedTitle.textContent = title;
+  const headerDate = document.createElement("date");
+  headerDate.textContent = date;
+  const firstP = document.createElement('firstParagraph');
+  firstP.textContent = firstParagraph;
+  const secondP = document.createElement('secondparagraph');
+  secondP.textContent = secondParagraph;
+  const thirdP = document.createElement('thirdParagraph');
+  thirdP.textContent = thirdParagraph;
+}
